2732b2e1f53edddf7c458dc043ae94b8007aacdd
Andrew D Smith <andrewds@usc.edu>
2025-02-25 14:23:46 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
-/*#include <stdio.h>
-int main() { printf("hello\n"); return 0;}*/
+#include <stdio.h>
+int main() { printf("hello\n"); return 0;}
 
-#include <ncurses.h>
+/*#include <ncurses.h>
 
 int main() {
     initscr();       // Initialize the screen
     endwin();        // End the ncurses session
     return 0;
 }
+*/
